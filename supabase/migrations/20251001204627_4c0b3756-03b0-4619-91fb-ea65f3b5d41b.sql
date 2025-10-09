18eba1779d810aa0f164bcc40cde1a367583c60b
saquetto-developer <saquettogithub@gmail.com>
2025-10-09 13:41:25 -0300

    Delete supabase directory

--- supabase/migrations/20251001204627_4c0b3756-03b0-4619-91fb-ea65f3b5d41b.sql
+++
@@ -1,6 +0,0 @@
--- Add foreign key constraint between saquetto_invoices_storage and saquetto
-ALTER TABLE saquetto_invoices_storage 
-ADD CONSTRAINT fk_saquetto_invoices_storage_saquetto_id 
-FOREIGN KEY (saquetto_id) 
-REFERENCES saquetto(id) 
-ON DELETE CASCADE;
