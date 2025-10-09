5a0b40e0c7f4df0f88a70ad4329009e4bd1e5a39
saquetto-developer <saquettogithub@gmail.com>
2025-10-09 16:12:27 -0300

    Delete supabase directory

--- supabase/migrations/20251001205608_4e2231ee-e3bf-4f77-87db-637ce611dc88.sql
+++
@@ -1,5 +0,0 @@
--- Corrigir file_path para usar caminho relativo ao invés de URL completa
-UPDATE saquetto_invoices_storage 
-SET file_path = saquetto_id || '/' || file_name,
-    updated_at = now()
-WHERE file_path LIKE 'http%' OR file_path LIKE '//%';
